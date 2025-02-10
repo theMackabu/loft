3ee4c51a2f6d2f6a26babe56fe4992dad2ff4152
theMackabu <theMackabu@gmail.com>
2025-02-09 19:04:12 -0800

    refactor: remove scoping module and simplify program execution flow
    The scoping module and its functionality were removed as the scope resolution
    will be handled during bytecode generation phase. Main now accepts file path
    as a command line argument instead of hardcoded test file.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
 mod ast;
 mod lexer;
 mod parser;
-mod scoping;
 mod util;
 
 use lexer::Lexer;
 use parser::Parser;
-use scoping::resolve_program;
 
 use std::process::ExitCode;
 use std::{error::Error, fs};
 
 fn main() -> Result<ExitCode, Box<dyn Error>> {
-    let input = fs::read_to_string("tests/main.rt")?;
+    let args = std::env::args().collect::<Vec<String>>();
+    let input = fs::read_to_string(&args[1])?;
     let lexer = Lexer::new(input);
+
     let mut parser = Parser::new(lexer);
 
     match parser.parse_program() {
-        Ok(ast) => {
-            if let Err(e) = resolve_program(&ast) {
-                println!("Scope resolution error: {}", e);
-            } else {
-                println!("{:#?}", ast);
-            }
-        }
+        Ok(ast) => println!("{ast:#?}"),
         Err(err) => println!("Parse error: {err}"),
     }
 
