68ba26541b2508168d03d67404992ea899507a8d
theMackabu <theMackabu@gmail.com>
2025-03-05 10:53:21 -0800

    add macos sdk

--- .pierre/ci/build.ts
+++ .pierre/ci/build.ts
@@ -2,6 +2,14 @@ import { run } from 'pierre';
 import { getVersion } from '../version';
 import { upload_file } from '../upload';
 
+async function add_linkers() {
+	const appleSDK = 'https://github.com/roblabla/MacOSX-SDKs/releases/download/13.3/MacOSX13.3.sdk.tar.xz ';
+
+	await run('apt update && apt install tar mingw-w64 -y', { label: 'Add mingw-w64' });
+	await run(`curl -L ${appleSDK} | tar xJ`, { label: 'Add darwin' });
+	await run('export SDKROOT=$(pwd)/MacOSX13.3.sdk/ && export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=rust-lld');
+}
+
 async function add_toolchains() {
 	await run('rustup target add x86_64-pc-windows-gnu', { label: 'Add target windows (x86_64)' });
 	await run('rustup target add x86_64-apple-darwin', { label: 'Add target darwin (x86_64)' });
@@ -25,4 +33,4 @@ async function upload() {
 	await upload_file({ time, version, path: 'aarch64-apple-darwin' });
 }
 
-export default [add_toolchains, build, upload];
+export default [add_linkers, add_toolchains, build, upload];
