7e3ed7385fd88d00da9001e7d7e1d2bdc8fa111b
theMackabu <theMackabu@gmail.com>
2025-03-05 17:29:17 -0800

    update curl in ci

--- .pierre/ci/build.ts
+++ .pierre/ci/build.ts
@@ -6,7 +6,7 @@ async function add_linkers() {
 	const appleSDK = 'https://github.com/roblabla/MacOSX-SDKs/releases/download/13.3/MacOSX13.3.sdk.tar.xz';
 	const zigbuild = 'https://github.com/rust-cross/cargo-zigbuild/releases/download/v0.19.8/cargo-zigbuild-v0.19.8.x86_64-unknown-linux-musl.tar.gz';
 
-	await run('apt-get update && apt-get install mingw-w64 -y', { label: 'Add mingw-w64' });
+	await run('apt-get update && apt-get install curl mingw-w64 -y', { label: 'Add mingw-w64' });
 	await run(`curl -L ${zigbuild} | tar xJ -C /opt`, { label: 'Add cargo-zigbuild' });
 	await run(`curl -L ${appleSDK} | tar xJ -C /opt && export SDKROOT=/opt/MacOSX11.3.sdk`, { label: 'Add darwin sdk' });
 }
