dcf7d8ffc5a114160c7af960b41b9b89f5cf644e
theMackabu <theMackabu@gmail.com>
2025-03-05 18:59:07 -0800

    downgrade zig version to 0.10.1

--- .pierre/ci/build.ts
+++ .pierre/ci/build.ts
@@ -2,24 +2,24 @@ import { run } from 'pierre';
 import { getVersion } from '../version';
 import { upload_file } from '../upload';
 
-const ZIG_VERSION = '0.14.0';
+const ZIG_VERSION = '0.10.1';
 const ZIG_BUILD_VERSION = '0.19.8';
 
-const ZIG_BINARY = `https://ziglang.org/download/${ZIG_VERSION}/zig-linux-x86_64-${ZIG_VERSION}.tar.xz`;
+const ZIG_BINARY = `ziglang.org/download/${ZIG_VERSION}/zig-linux-x86_64-${ZIG_VERSION}.tar.xz`;
 const ZIG_BUILD_BINARY = `github.com/rust-cross/cargo-zigbuild/releases/download/v${ZIG_BUILD_VERSION}/cargo-zigbuild-v${ZIG_BUILD_VERSION}.x86_64-unknown-linux-musl.tar.gz`;
 
 const LD_LIBRARY_PATH = 'LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH';
 const APPLE_SDK = 'github.com/roblabla/MacOSX-SDKs/releases/download/13.3/MacOSX13.3.sdk.tar.xz';
 
 async function install_zig() {
-	await run(`curl -L ${ZIG_BINARY} | tar xJ -C /usr/local`, { label: `Install zig ${ZIG_VERSION}` });
-	await run(`ln -s "/usr/local/zig-linux-x86_64-${ZIG_VERSION}/zig" /usr/local/bin/zig`);
+	await run(`${LD_LIBRARY_PATH} curl -L "https://${ZIG_BINARY}" | tar xJ -C /usr/local`, { label: `Install zig ${ZIG_VERSION}` });
+	await run(`ln -s "/usr/local/zig-linux-x86_64-${ZIG_VERSION}/zig" /usr/local/bin/zig`, { label: 'Adding zig to /usr/local/bin' });
 }
 
 async function add_linkers() {
 	await run('apt-get update && apt-get install mingw-w64 -y', { label: 'Add mingw-w64' });
-	await run(`${LD_LIBRARY_PATH} curl -L https://${APPLE_SDK} | tar xJ -C /opt`, { label: 'Add darwin sdk' });
-	await run(`${LD_LIBRARY_PATH} curl -L https://${ZIG_BUILD_BINARY} | tar xz -C /opt`, { label: 'Add cargo-zigbuild' });
+	await run(`${LD_LIBRARY_PATH} curl -L "https://${APPLE_SDK}" | tar xJ -C /opt`, { label: 'Add darwin sdk' });
+	await run(`${LD_LIBRARY_PATH} curl -L "https://${ZIG_BUILD_BINARY}" | tar xz -C /opt`, { label: 'Add cargo-zigbuild' });
 }
 
 async function add_toolchains() {
